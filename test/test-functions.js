847b7dce5951f0e47110528e5b3ce3959e62859c
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-12-30 23:45:56 +0900

    Fix Fn::Join

---
+++ test/test-functions.js
@@ -0,0 +1,26 @@
+const test = require('tape');
+const { readTemplate } = require('cfn-read-template');
+const { makeTemplateParser } = require('..');
+
+test('test parseTemplate: functions', t => {
+  t.plan(2);
+
+  readTemplate(`${__dirname}/test-functions.yaml`, (err, template) => {
+    t.error(err);
+
+    const context = {
+      Parameters: {
+        'AWS::AccountId': {
+          Value: '123456789012',
+        },
+        'AWS::Region': {
+          Value: 'us-west-2',
+        },
+      },
+    };
+
+    makeTemplateParser()(template, context, (err, result) => {
+      t.error(err);
+    });
+  });
+});
